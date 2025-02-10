8d94ebeee6ac0d49c2cf93a69066cd1b610039ba
Stanislav Surskiy <ssurskiy@gmail.com>
2025-02-11 01:44:43 +0300

    test(BaseTest): add base test class with fixture loading
    - Introduce `BaseTest` class extending `TestCase`
    - Add `testIfTrue` method to verify basic test functionality
    - Implement `loadFixtures` method for loading JSON fixtures from a specified directory

---
+++ tests/BaseTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Muscobytes\Poizon\tests;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(BaseTest::class)]
+class BaseTest extends TestCase
+{
+    /**
+     * @covers BaseTest
+     */
+    public function testIfTrue()
+    {
+        $this->assertTrue(true);
+    }
+
+
+    protected static function loadFixtures(string $folder): array
+    {
+        $result = [];
+        $fixturesPath = __DIR__ . '/fixtures/' . $folder . '/';
+        if ($handle = opendir($fixturesPath)) {
+            while (false !== ($entry = readdir($handle))) {
+                if (!in_array($entry, ['.', '..', '.DS_Store'])) {
+                    $result[]['entry'] = json_decode(file_get_contents($fixturesPath . $entry), true);
+                }
+            }
+            closedir($handle);
+        }
+        return $result;
+
+    }
+}
