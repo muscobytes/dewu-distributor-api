dd2610d37fcb7a318993f06364eb4298ef76c995
Stanislav Surskiy <ssurskiy@gmail.com>
2025-02-11 00:41:16 +0300

    docs(QuerySpuListParameters): add parameter descriptions
    - Add docblock with detailed descriptions for constructor parameters in `QuerySpuListParameters.php`
    - Improve code readability and maintainability by providing context for each parameter

--- src/Dto/Distribute/Product/QuerySpuList/QuerySpuListParameters.php
+++ src/Dto/Distribute/Product/QuerySpuList/QuerySpuListParameters.php
@@ -6,6 +6,25 @@ use Muscobytes\Poizon\DistributionApiClient\Abstract\Parameters;
 
 class QuerySpuListParameters extends Parameters
 {
+    /**
+     * @param int $startId Pagination start ID
+     * @param int $pageSize Page size
+     * @param array $pwSpuId SPU ID
+     * @param array $distPuId Distributor's SPU ID
+     * @param string $dwSpuTitle Item name in Chinese
+     * @param string $distPusTitle Item name in English
+     * @param array $dwDesignerId Article number/Style ID
+     * @param array $distBrandName Brand name
+     * @param array $distCategory1Name Primary category
+     * @param array $distCategory2Name Secondary category
+     * @param array $distCategory3Name Tertiary category
+     * @param array $distFitPeopleName Target user
+     * @param string $season Season
+     * @param string $distStatus Status (PRODUCT_ON: Published, PRODUCT_OFF: Removed)
+     * @param int $modifyStartTime Modification start time, UTC timestamp in milliseconds
+     * @param int $modifyEndTime Modification end time, UTC timestamp in milliseconds
+     * @param bool $querySku Return SKU info (Y or N)
+     */
     public function __construct(
         public int $startId,
         public int $pageSize,
