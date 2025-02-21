a282226316c2cdc33194fa2f5bfd1b749d3e1d1f
Stanislav Surskiy <ssurskiy@gmail.com>
2025-02-21 21:57:54 +0300

    test(httpmethods): add `PutStub` for unit testing
    - Introduce `PutStub` class to utilize the `Put` trait for testing purposes
    - Refactor `PutTest` to use `PutStub` instead of `PutTestClass`
    - Simplifies the unit test setup and aligns with test conventions

---
+++ tests/Stubs/Traits/HttpsMethods/PutStub.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Muscobytes\Poizon\DistributionApiClient\Tests\Stubs\Traits\HttpsMethods;
+
+use Muscobytes\Poizon\DistributionApiClient\Traits\HttpMethods\Put;
+
+class PutStub
+{
+    use Put;
+}
--- tests/Unit/Traits/HttpMethods/PutTest.php
+++ tests/Unit/Traits/HttpMethods/PutTest.php
@@ -4,21 +4,17 @@ declare(strict_types=1);
 
 namespace Muscobytes\Poizon\DistributionApiClient\Tests\Unit\Traits\HttpMethods;
 
+use Muscobytes\Poizon\DistributionApiClient\Tests\Stubs\Traits\HttpsMethods\PutStub;
 use Muscobytes\Poizon\DistributionApiClient\Traits\HttpMethods\Put;
 use Muscobytes\Poizon\DistributionApiClient\Tests\BaseTest;
 use PHPUnit\Framework\Attributes\CoversClass;
-
-class PutTestClass
-{
-    use Put;
-}
 
 #[CoversClass(Put::class)]
 class PutTest extends BaseTest
 {
     public function testPutTrait(): void
     {
-        $class = new PutTestClass();
+        $class = new PutStub();
         $this->assertEquals('PUT', $class->getHttpMethod());
     }
 }
